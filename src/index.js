d50bc9a145937a7bb254b9f30e25affb3fd43e9d
Steven Rosario <strosariogarcia@gmail.com>
2022-08-31 14:54:56 -0400

    Did the deliverables and delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener("submit", (e) => {
+    e.preventDefault();
+    const som = document.getElementById("new-task-description");
+    if(som.value !== ""){
+      addToDOM(som.value)
+    }
+  })
 });
+
+function addToDOM(something){
+
+  // Creates a list item and adds its text content
+  let visibleText = document.createElement('li');
+  visibleText.textContent = something + ' ';
+  
+  // Creates a button next to each task to delete
+  // Side note for me: Remember button will not work without an addEventListener :)
+  let btn = document.createElement('button');
+  btn.textContent = 'X';
+  btn.addEventListener('click', deleteFromDOM);
+
+  visibleText.appendChild(btn);
+  console.log(visibleText);
+
+  
+  document.querySelector("#tasks").appendChild(visibleText);
+}
+
+function deleteFromDOM(e){
+  // As the name states, it will delete from the ul parentNode
+  e.target.parentNode.remove();
+}
